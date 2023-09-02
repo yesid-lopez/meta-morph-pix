54ed9b72f2a9b4036cb18dbc16aacdb655cbd644
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2023-09-02 23:21:17 +0200

    feat: adds check if file exists

--- meta_morph_pix/router.py
+++ meta_morph_pix/router.py
@@ -15,17 +15,22 @@ async def healthcheck():
     bucket = cl.get_bucket_by_id("76ac3dd4126f93fb8a6f061e")
     for file_version, _ in bucket.ls(recursive=True):
         print(file_version.file_name)
-    return {"Hello": "World"}
+    return {"Hello": "Pano Rabbit"}
 
 
 @app.post("/transform_images")
 async def transform_image(folder_name: str, files: list[UploadFile]):
+    messages = []
     for file in files:
         transformed_image = await transformation_service.transform_image(file)
-        storage_service.upload_image(transformed_image, f"{folder_name}/{file.filename}")
-    return {"status": "Images uploaded successfully"}
+        try:
+            filename = storage_service.upload_image(transformed_image, f"{folder_name}/{file.filename}")
+            messages.append(f"Image {filename} uploaded successfully")
+        except FileExistsError as error:
+            messages.append(f"{str(error)}")
+    return {"data": messages}
 
 
 def run():
-    host = os.getenv("HOST")
+    host = os.getenv("HOST", "127.0.0.1")
     uvicorn.run("meta_morph_pix.router:app", host=host, reload=True)
--- meta_morph_pix/services/storage_service.py
+++ meta_morph_pix/services/storage_service.py
@@ -1,9 +1,26 @@
+from b2sdk.v2.exception import FileNotPresent
+
 from ..config import backblaze
 from ..utils.blackblaze_client import get_b2_client
 
 
-def upload_image(buffer, image_name):
+def upload_image(buffer, image_name) -> str:
     b2_client = get_b2_client()
     bucket = b2_client.get_bucket_by_name(backblaze.bucket_name)
-    response = bucket.upload_bytes(buffer.getvalue(), f"transformed/{image_name}", content_type='image/jpeg')
-    print(b2_client.get_file_info(response.id_))
+    file_path = f"transformed/{image_name}"
+
+    if check_file_exists(file_path):
+        raise FileExistsError(f"Image {file_path} already exists")
+
+    response = bucket.upload_bytes(buffer.getvalue(), file_path, content_type='image/jpeg')
+    return response.file_name
+
+
+def check_file_exists(file_name) -> bool:
+    b2_client = get_b2_client()
+    bucket = b2_client.get_bucket_by_name(backblaze.bucket_name)
+    try:
+        bucket.get_file_info_by_name(file_name)
+        return True
+    except FileNotPresent:
+        return False
